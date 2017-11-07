b605d5eca47078e1f5614873c719ffe58b443530
Robert Cohen <rcohen0511@gmail.com>
2017-11-06 22:25:04 -0500

    Everything works except get functions, need to add styling

--- sms-service.js
+++ sms-service.js
@@ -30,15 +30,31 @@ http.createServer(app).listen(process.env.PORT || 3000, function () {
 app.post('/sms', function (request, response) {
     var msgBody = request.body.Body;
     var userPhone = request.body.From;
+    var question = getQuestion();
+    console.log(question)
 
-    console.log(checkRegistration(userPhone))
+// NEED TO ADD && hasTakenQuiz(userPhone) to each else if
+
 
     if(msgBody.toLowerCase().trim() == 'join'){
         addUserToSql(userPhone);
-    } else if(msgBody.toLowerCase().trim() == 'start quiz'){
-        // takeQuiz(userPhone);
-        console.log('Take Quiz')
-    }else{
+    } else if(msgBody.toLowerCase().trim() == 'start quiz'  && checkRegistration(userPhone)){
+        console.log('Sending Quiz Question');
+        sendQuestion(userPhone);
+    } else if(msgBody.toLowerCase().trim()== question[1] && hasQuizStarted(userPhone)){
+        console.log('Answer is correct');
+        sendCorrectResponse(userPhone,question[1]);
+        updateSQL(userPhone,question[1],true)
+    } else if(msgBody.toLowerCase().trim()== question[2] && hasQuizStarted(userPhone)){
+        console.log('Answer is wrong');
+        sendIncorrectResponse(userPhone,question[2],question[1]);
+        updateSQL(userPhone,question[2],false)
+    } else if(msgBody.toLowerCase().trim()== question[3] && hasQuizStarted(userPhone)){
+        console.log('Answer is wrong');
+        sendIncorrectResponse(userPhone,question[3],question[1]);
+        updateSQL(userPhone,question[3],false)
+
+    } else{
         InvalidUserInputText(userPhone, msgBody)
     }
 });
@@ -52,23 +68,33 @@ function startLesson(){
     // Set who wants quiz to false
     var phoneNumbers = getPhoneNumbers();
     sendInformationText(phoneNumbers);
-    // Info message should end with do you want to take a quiz?
+    // Info message should end with do you want to take a quiz?    
 }
 
 function sendInformationText(phoneNumbers){
     // loops through numbers
-
 }
 
 function checkRegistration(phonenumber){
     // Check if user exists in DB
-    checkNumberExists(phonenumber)
+    // checkNumberExists(phonenumber)
+    return true;
 
 }
 
-function takeQuiz(phonenumber){
-    // Modify DB to reflect that user is taking the quiz
+function sendQuestion(phonenumber){
+    var question = getQuestion();
+    console.log('User : '+phonenumber+' is starting a quiz')
+    client.messages.create({
+        from: '+19149966800',
+        to: phonenumber,
+        body: question[0]+":\n"+question[1]+"\n"+question[2]+"\n"+question[3]
+    }, function (err, message) {
+        if (err) console.error(err.message);
+    });
 }
+
+
 
 // ================================================================
 // Admin Console
@@ -104,9 +130,9 @@ function displayForm(res) {
     });
 };
 
-var values = [];
 function formSubmission(req, res) {
     // Setting up Form
+    var values = [];
     var fields = [];
     var form = new formidable.IncomingForm();
     form.on('field', function (field, value) {
@@ -122,12 +148,13 @@ function formSubmission(req, res) {
             fields: fields
         }));
 
-    // Printing Out Form
-        console.log(values);
+    // // Printing Out Form
+    //     console.log(fields);
+    //     console.log(values);
+        addQuestionsToSql(values);
     });
     form.parse(req);
 }
-
 // ================================================================
 // Twillio Messages
 // ================================================================
@@ -137,6 +164,28 @@ function InvalidUserInputText(phonenumber, text){
         from: '+19149966800',
         to: phonenumber,
         body: "Please check your spelling, if you have not joined the class text 'join' to start learning!"
+    }, function (err, message) {
+        if (err) console.error(err.message);
+    });
+}
+
+function sendCorrectResponse(phonenumber, answer){
+    console.log('User inserted correct answer: '+answer)
+    client.messages.create({
+        from: '+19149966800',
+        to: phonenumber,
+        body: "Congratulations you answered this correctly!"
+    }, function (err, message) {
+        if (err) console.error(err.message);
+    });
+}
+
+function sendIncorrectResponse(phonenumber, answer, correctAnswer){
+    console.log('User inserted incorrect answer: '+answer)
+    client.messages.create({
+        from: '+19149966800',
+        to: phonenumber,
+        body: "Sorry you got this one wrong, the correct answer was: "+correctAnswer
     }, function (err, message) {
         if (err) console.error(err.message);
     });
@@ -207,8 +256,6 @@ function addUserToSql(phonenumber) {
     successfullyAddedText(phonenumber);
 }
 
-
-var exists = false;
 function checkNumberExists(phonenumber){
     console.log('Checking if number: '+phonenumber+' is in DB')
     var mysql = require('mysql');
@@ -238,4 +285,95 @@ function checkNumberExists(phonenumber){
     con.end();
 }
 
+
+function addQuestionsToSql(data) {
+    // Connection to SQL
+    var mysql = require('mysql');
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "123456",
+        database: 'db'
+    });
+    con.connect((err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Connection established');
+    });
+
+    // Insert Statement
+    con.query("delete from questions where question like '%%' ")
+    con.query('insert into questions (question, answer, option1, option2) value (?)', [data], function (error, rows, fields) {
+        if (error) throw error;
+    });
+    // Close connection
+    con.end();
+    // END SQL
+    console.log('Successfully Added data into SQL: '+data)
+}
+
+function updateSQL(phonenumber, answer, bool){
+    // Connection to SQL
+    console.log('Updating User DB, answer: '+answer+' ,correct: '+bool)
+    var mysql = require('mysql');
+    var con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "123456",
+        database: 'db'
+    });
+    con.connect((err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Connection established');
+    });
+
+    // Insert Statement
+    con.query("UPDATE class SET answer = (?), answeredCorrectly = (?) where phonenumber = (?)", [answer, bool, phonenumber], function (error, rows, fields) {
+        if (error) throw error;
+    });
+    // Close connection
+    con.end();
+    // END SQL
+}
+
+// Get From DB functions
+// ================================================================
+// need to fix
+function getPhoneNumbers(){
+    // TODO Add SQL query
+    numbers = ['+19143301533','+19174160409']
+    return numbers
+}
+
+//Need to fix
+function getQuestion(){
+    return ['q3','a','1','2'];
+}
+
+//Need to fix
+function hasQuizStarted(phonenumber){
+    return true;
+}
+
+//Need to fix
+function hasTakenQuiz(phonenumber){
+    return true;
+}
+
+// Jon will finish this function and create an HTML table that will be shown after
+// form submission:
+// Make a button to refresh
+// Listview 
+// sound/video
+// 1 link 
+// 3 images
+
+function getClassGrades(){
+    return 
+}
 
